da89368664a16e391fe63a808aea17988a7fca18
Mr5niper <davidx.l.johnson@gmail.com>
2026-02-10 02:49:56 -0800

    Updated version info to 1.5.0.0

--- audioctl/gui.py
+++ audioctl/gui.py
@@ -376,7 +376,7 @@ def run_audioctl_interactive(args_list, prompt_patterns, expect_ok=True):
 class AudioGUI:
     def __init__(self, root):
         self.root = root
-        self.root.title("Mr5niper's Audio Control  v1.4.7.3  02-06-2026")
+        self.root.title("Mr5niper's Audio Control  v1.5.0.0  02-10-2026")
         # Style and theme
         style = ttk.Style(self.root)
         try:
@@ -2184,3 +2184,4 @@ def launch_gui():
         _log_exc("MAINLOOP EXCEPTION")
     _log("launch_gui: mainloop exited")
     return 0
+
